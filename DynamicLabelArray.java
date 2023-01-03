9a6e67a8866e0e7fa3cb5e27aa7d112e082ef55b
DamianLHr <119448632+DamianLHr@users.noreply.github.com>
2023-01-03 15:33:34 +0200

    Create DynamicLabelArray.java
    This is the DynamicLabelArray that can be used outside of the BuildMenu

---
+++ DynamicLabelArray.java
@@ -0,0 +1,79 @@
+class DynamicLabelArray {
+
+        private JLabel[] labels = new JLabel[0];
+
+        public DynamicLabelArray(int length) {
+            labels = new JLabel[length];
+            for (int i = 0; i < labels.length; i++) {
+                labels[i] = new JLabel();
+                labels[i].setBounds(0, 0, 1, 1);
+            }
+        }
+
+        public void setlabels(JLabel[] arr) {
+            for (int i = 0; i < labels.length; i++) {
+                this.labels[i] = arr[i];
+            }
+        }
+
+        public void addLabel() {
+            JLabel[] arr = new JLabel[labels.length + 1];
+            for (int i = 0; i < labels.length; i++) {
+                arr[i] = labels[i];
+
+            }
+            labels = new JLabel[arr.length];
+            setlabels(arr);
+        }
+
+        public void removeLabel(int elementNumber) {
+
+            if (elementNumber >= 0 && elementNumber < labels.length) {
+                JLabel[] arr = new JLabel[labels.length - 1];
+                for (int i = 0, y = 0; i < labels.length; i++, y++) {
+
+                    if (i == elementNumber) {
+                        labels[i].setVisible(false);
+
+                        if (elementNumber == labels.length - 1) {
+
+                        } else {
+                            y--;
+                        }
+                    } else {
+                        arr[y] = labels[i];
+                    }
+
+                }
+                labels = new JLabel[arr.length];
+                setlabels(arr);
+
+            } else {
+                System.out.println("Error: not able to remove element out of array length");
+            }
+        }
+
+        public int length() {
+            return labels.length;
+        }
+
+        public void spawnShip(int numberInArray, int x, int y) {
+            labels[numberInArray] = new JLabel();
+            labels[numberInArray].setIcon((Icon) labelGhostIcon);
+            labels[numberInArray].setBounds(x, y, width, height);
+            labels[numberInArray].setOpaque(false);
+        }
+
+        public void boundsSetter(int numberInArray, int x, int y, int width, int height) {
+            labels[numberInArray].setBounds(x, y, width, height);
+        }
+
+        public void iconSetter(int numberInArray, Icon icon) {
+            labels[numberInArray].setIcon(icon);
+        }
+
+        public JLabel elementGetter(int numberInArray) {
+            return labels[numberInArray];
+        }
+
+    }
