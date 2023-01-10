a2342f54559598843f7bcdcb07e45c800103c385
Bosron <59515255+Bosron@users.noreply.github.com>
2023-01-10 23:32:09 +0200

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -11,12 +11,12 @@ import javax.swing.WindowConstants;
 public class MainMenu extends javax.swing.JFrame {
 
     private JLabel background = new JLabel();
-    private JLabel title = new JLabel();
-    private JLabel start = new JLabel();
-    private JLabel rules = new JLabel();
-    private JLabel credits = new JLabel();
-    private JLabel options = new JLabel();
-    private JLabel quit = new JLabel();
+    private JLabel txtTitle = new JLabel();
+    private JLabel startButton = new JLabel();
+    private JLabel rulesButton = new JLabel();
+    private JLabel creditsButton = new JLabel();
+    private JLabel optionsButton = new JLabel();
+    private JLabel quitButton = new JLabel();
     private JLabel buttonBackdrop = new JLabel();
     private JLayeredPane layeredPane = new JLayeredPane();
 
@@ -33,17 +33,17 @@ public class MainMenu extends javax.swing.JFrame {
         background.setOpaque(false);
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="title">
-        title.setIcon(new ImageIcon("src/images/Title.png"));
-        title.setBounds(47, 50, 806, 120);
-        title.setOpaque(false);
+        // <editor-fold defaultstate="collapsed" desc="txtTitle">
+        txtTitle.setIcon(new ImageIcon("src/images/Title.png"));
+        txtTitle.setBounds(47, 50, 806, 120);
+        txtTitle.setOpaque(false);
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="start">
-        start.setIcon(new ImageIcon("src/images/start.png"));
-        start.setBounds(350, 330, 200, 53);
-        start.setOpaque(false);
-        start.addMouseListener(new MouseAdapter() {
+        // <editor-fold defaultstate="collapsed" desc="startButton">
+        startButton.setIcon(new ImageIcon("src/images/start.png"));
+        startButton.setBounds(350, 330, 200, 53);
+        startButton.setOpaque(false);
+        startButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 disposer();
@@ -53,11 +53,11 @@ public class MainMenu extends javax.swing.JFrame {
         });
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="rules">
-        rules.setIcon(new ImageIcon("src/images/rules.png"));
-        rules.setBounds(379, 403, 142, 50);
-        rules.setOpaque(false);
-        rules.addMouseListener(new MouseAdapter() {
+        // <editor-fold defaultstate="collapsed" desc="rulesButton">
+        rulesButton.setIcon(new ImageIcon("src/images/rules.png"));
+        rulesButton.setBounds(379, 403, 142, 50);
+        rulesButton.setOpaque(false);
+        rulesButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 JOptionPane.showMessageDialog(null, "neshto si pravila", "Playing rules", JOptionPane.INFORMATION_MESSAGE);
@@ -65,11 +65,11 @@ public class MainMenu extends javax.swing.JFrame {
         });
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="credits">
-        credits.setIcon(new ImageIcon("src/images/credits.png"));
-        credits.setBounds(360, 473, 180, 50);
-        credits.setOpaque(false);
-        credits.addMouseListener(new MouseAdapter() {
+        // <editor-fold defaultstate="collapsed" desc="creditsButton">
+        creditsButton.setIcon(new ImageIcon("src/images/credits.png"));
+        creditsButton.setBounds(360, 473, 180, 50);
+        creditsButton.setOpaque(false);
+        creditsButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 JOptionPane.showMessageDialog(null, "credits", "Credits", JOptionPane.INFORMATION_MESSAGE);
@@ -77,11 +77,11 @@ public class MainMenu extends javax.swing.JFrame {
         });
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="options">
-        options.setIcon(new ImageIcon("src/images/options.png"));
-        options.setBounds(362, 543, 175, 53);
-        options.setOpaque(false);
-        options.addMouseListener(new MouseAdapter() {
+        // <editor-fold defaultstate="collapsed" desc="optionsButton">
+        optionsButton.setIcon(new ImageIcon("src/images/options.png"));
+        optionsButton.setBounds(362, 543, 175, 53);
+        optionsButton.setOpaque(false);
+        optionsButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 JOptionPane.showMessageDialog(null, "Under Construction!", "///////////////////", JOptionPane.WARNING_MESSAGE);
@@ -89,11 +89,11 @@ public class MainMenu extends javax.swing.JFrame {
         });
         // </editor-fold>
 
-        // <editor-fold defaultstate="collapsed" desc="quit">
-        quit.setIcon(new ImageIcon("src/images/quit.png"));
-        quit.setBounds(407, 614, 85, 44);
-        quit.setOpaque(false);
-        quit.addMouseListener(new MouseAdapter() {
+        // <editor-fold defaultstate="collapsed" desc="quitButton">
+        quitButton.setIcon(new ImageIcon("src/images/quit.png"));
+        quitButton.setBounds(407, 614, 85, 44);
+        quitButton.setOpaque(false);
+        quitButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
                 disposer();
@@ -107,12 +107,12 @@ public class MainMenu extends javax.swing.JFrame {
         buttonBackdrop.setOpaque(false);
         // </editor-fold>
 
-        layeredPane.add(title, Integer.valueOf(2));
-        layeredPane.add(start, Integer.valueOf(2));
-        layeredPane.add(rules, Integer.valueOf(2));
-        layeredPane.add(credits, Integer.valueOf(2));
-        layeredPane.add(options, Integer.valueOf(2));
-        layeredPane.add(quit, Integer.valueOf(2));
+        layeredPane.add(txtTitle, Integer.valueOf(2));
+        layeredPane.add(startButton, Integer.valueOf(2));
+        layeredPane.add(rulesButton, Integer.valueOf(2));
+        layeredPane.add(creditsButton, Integer.valueOf(2));
+        layeredPane.add(optionsButton, Integer.valueOf(2));
+        layeredPane.add(quitButton, Integer.valueOf(2));
         layeredPane.add(buttonBackdrop, Integer.valueOf(2));
         layeredPane.add(background, Integer.valueOf(0));
         this.add(layeredPane);
